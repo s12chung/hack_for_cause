6b20f9b39ae997638e62434ba08f28c6ca9495c8
s12chung <main@mba-steve.local>
2011-11-23 20:24:45 -0500

    done dashboard

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -5,15 +5,15 @@ $(document).ready(function() {
     setInterval(function() {
         number_container.innerHTML = '';
 
-//        startingNum += Math.floor(Math.random()*10);
-//
-//        var string = startingNum.toString();
-//        var i;
-//        for(i=0; i< string.length -1; i++) {
-//            var number = document.createElement('div');
-//            number.className = "number";
-//            number.innerHTML = string.charAt(i);
-//            number_container.appendChild(number);
-//        }
+        startingNum += Math.floor(Math.random()*10);
+
+        var string = startingNum.toString();
+        var i;
+        for(i=0; i< string.length -1; i++) {
+            var number = document.createElement('div');
+            number.className = "number";
+            number.innerHTML = string.charAt(i);
+            number_container.appendChild(number);
+        }
     }, 2000);
 });
