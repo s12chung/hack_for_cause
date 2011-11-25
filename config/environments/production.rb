6d80f2847dd30698345d1015cbf7499d95f49fde
s12chung <main@mba-steve.(none)>
2011-11-24 19:05:08 -0500

    email fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,6 +58,7 @@ HackForCause::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
+  config.action_mailer.default_url_options = { :host => "hackforacause.herokuapp.com" }
   ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
